626c3bba982447a36a048d025b1b64d8337e7db9
Eric Potash <eric@k2co3.net>
2019-04-15 16:58:51 -0500

    more assessor address fixes

--- lead/aux/parcel_address.sql
+++ lead/aux/parcel_address.sql
@@ -5,10 +5,15 @@ create table aux.parcel_address as (
         "PIN", house_num, st_dir, st_name, st_suffix,
             substring(house_num from 6)::int || ' ' || coalesce(st_dir || ' ', '') ||
                case when st_name = 'SAINT LAWRENCE' then 'ST LAWRENCE'
-                    when st_name = 'MARTIN LUTHER KING' then 'DR MARTIN LUTHER KING JR'
+                    when st_name = 'SAINT LOUIS' then 'ST LOUIS'
+                    when st_name in ('MARTIN LUTHER KING', 'KING', 'SOUTH PARK') then 'DR MARTIN LUTHER KING JR'
+                    when st_name = 'LASALLE' then 'LA SALLE'
                     else st_name end || 
                 -- some sts don't have a suffice, like "AVENUE A"
-                coalesce(' ' || regexp_replace(st_suffix, '^PKY$', 'PKWY'), '') as address
+                coalesce(' ' || 
+                    case when st_suffix = 'PKY' then 'PKWY' 
+                         when st_name = 'SOUTH PARK' then 'DR' 
+                    end, '') as address
     from input.assessor
     where city = 'CHICAGO' and st_name is not null
     order by 1
